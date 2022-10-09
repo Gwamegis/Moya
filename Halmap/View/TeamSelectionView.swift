0bd0f4ef89c07fc51cfdfc6760ff9c52e7ebc0be
Thina <frog7134@gmail.com>
2022-10-09 11:23:21 +0900

    [Feat] #10 teamselectionview에 팀 선택 UI 추가

--- Halmap/View/TeamSelectionView.swift
+++ Halmap/View/TeamSelectionView.swift
@@ -2,14 +2,37 @@
 //  TeamSelectionView.swift
 //  Halmap
 //
-//  Created by Yeni Hwang on 2022/10/08.
+//  Created by 유정인 on 2022/10/08.
 //
 
 import SwiftUI
 
 struct TeamSelectionView: View {
+    var columns: [GridItem] = Array(repeating: .init(.adaptive(minimum: 200, maximum: .infinity), spacing: 20), count: 2)
+    var teamLogo: [String] = ["doosan", "lotte", "hanwha", "kiwoom", "kia", "ssg"]
+
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(alignment: .leading, spacing: 40) {
+            Text("""
+            당신의 \(Text("팀").font(.system(size: 30, weight: .bold)).foregroundColor(.red))을
+            \(Text("골라주세요!").font(.system(size: 30, weight: .bold)))
+            """)
+            .font(.system(size: 26, weight: .regular))
+            .padding(.leading, 11)
+            LazyVGrid(columns: columns, spacing: 20) {
+                ForEach(teamLogo, id: \.self) { i in
+                    Button {
+                        // TODO: - 팀 선택시 효과 추가
+                    } label: {
+                        Image(i)
+                            .resizable()
+                            .frame(width: 165, height: 158)
+                            .clipShape(RoundedRectangle(cornerRadius: 8))
+                    }
+                }
+            }
+        }
+        .padding(.horizontal, 20)
     }
 }
 
