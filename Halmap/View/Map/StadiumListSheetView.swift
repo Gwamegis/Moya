fac4fa9b8dea2a90831efa3d752822ff37fd1bb0
Anti9uA <didlaak6000@naver.com>
2022-10-09 11:00:41 +0900

    [feat] #1 경기장 시트 만들기

--- Halmap/View/Map/StadiumListSheetView.swift
+++ Halmap/View/Map/StadiumListSheetView.swift
@@ -2,16 +2,37 @@
 //  StadiumListSheetView.swift
 //  Halmap
 //
-//  Created by Yeni Hwang on 2022/10/08.
+//  Created by Kyubo Shim on 2022/10/08.
 //
 
 import SwiftUI
 
 struct StadiumListSheetView: View {
+    @Environment(\.presentationMode) var presentationMode: Binding<PresentationMode>
+    private let messages = [ "서울 종합 야구장", "고척 스카이돔", "SSG 랜더스 필드", "사직 야구장", "대전 이글스 파크", "기아 챔피언스필드"]
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationView {
+            VStack {
+                Text("야구장 지도")
+                    .fontWeight(.heavy)
+                    .padding(EdgeInsets(top: 20, leading: 0, bottom: 40, trailing: 0))
+                //                    .padding(.trailing, UIScreen.main.bounds.width - 120)
+                
+                
+                List(messages, id: \.self) { message in
+                    NavigationLink(destination: StadiumMapView(message: message)) {
+                        Text(message)
+                            .padding(EdgeInsets(top: 10, leading: 0, bottom: 10, trailing: 0))
+                    }
+                }
+                .listStyle(.plain)
+            }
+            .navigationBarHidden(true)
+        }
     }
 }
+
 
 struct StadiumListSheetView_Previews: PreviewProvider {
     static var previews: some View {
