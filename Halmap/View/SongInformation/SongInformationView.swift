11bf14593fb24572ea31fdc4a69790b15dbf6647
JIWON1923 <zest1923@gmail.com>
2022-11-05 20:51:54 +0900

    [Feat] #50 - set lyrics alignment

--- Halmap/View/SongInformation/SongInformationView.swift
+++ Halmap/View/SongInformation/SongInformationView.swift
@@ -13,17 +13,19 @@ struct SongInformationView: View {
     @State var music: Music
     
     var body: some View {
-        VStack{
+        VStack(alignment: .leading) {
+            
             SongHeaderView(music: $music)
                 .frame(width: UIScreen.main.bounds.width + 3,
                        height: 156)
                 .background(Color("songGrey"))
             
             SongContentView(music: $music)
-                .frame(width: UIScreen.main.bounds.width + 3)
                 .background(.white)
+            
             Spacer()
-        }.ignoresSafeArea()
+        }
+        .ignoresSafeArea()
     }
 }
 
