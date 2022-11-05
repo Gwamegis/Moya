add0112e1fea1909f8bde75487c0e4d3598e9f0d
Elie (Jeon Jimin) <cjjunn0827@gmail.com>
2022-11-05 22:20:29 +0900

    [Feat] create fetch function

--- Halmap/Data/Song.swift
+++ Halmap/Data/Song.swift
@@ -9,14 +9,9 @@ import Foundation
 
 struct Song: Identifiable, Codable {
     var id: String
-    var team: String
     var type: Bool
     var title: String
     var lyrics: String
     var info: String
-    
-    var dictionary: [String: Any] {
-        let data = (try? JSONEncoder().encode(self)) ?? Data()
-        return (try? JSONSerialization.jsonObject(with: data, options: .mutableContainers) as? [String: Any]) ?? [:]
-    }
+    var url: String
 }
