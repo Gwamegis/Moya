7ab69d7d1682fec2f15acbfd8929e0974d8085aa
JIWON1923 <zest1923@gmail.com>
2022-11-06 00:11:26 +0900

    [Feat] update gesture
    Co-authored-by: Kyubo Shim <didlaak6000@naver.com>

---
+++ Halmap/Extensions/Halmap+UINavigationController.swift
@@ -0,0 +1,19 @@
+//
+//  Halmap+UINavigationController.swift
+//  Halmap
+//
+//  Created by 이지원 on 2022/11/06.
+//
+
+import UIKit
+
+extension UINavigationController: ObservableObject, UIGestureRecognizerDelegate {
+    override open func viewDidLoad() {
+        super.viewDidLoad()
+        interactivePopGestureRecognizer?.delegate = self
+    }
+    
+    public func gestureRecognizerShouldBegin(_ gestureRecognizer: UIGestureRecognizer) -> Bool {
+        return viewControllers.count > 1
+    }
+}
