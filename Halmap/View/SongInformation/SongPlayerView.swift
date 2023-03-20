7ee6f6f2c2cabf729247a15896fc09379b2ff1d6
yeni <dev.yeeun@gmail.com>
2023-03-21 02:04:02 +0900

    [Feat] #96 - AudioManager 추가

--- Halmap/View/SongInformation/SongPlayerView.swift
+++ Halmap/View/SongInformation/SongPlayerView.swift
@@ -13,55 +13,111 @@ struct SongPlayerView: View {
     @State var selectedTeam: String = (UserDefaults.standard.string(forKey: "selectedTeam") ?? "test")
     @Binding var song: Song
     @State var sound: Data?
-    @State var audioPlayer: AVPlayer!
+    @State var player = AVPlayer()
     @State var isPlaying: Bool = true
     @Environment(\.presentationMode) var presentationMode
     
+    @State var value: Double = 0.0
+    @EnvironmentObject var audioManager: AudioManager
+    let timer = Timer
+        .publish(every: 0.3, on: .main, in: .common)
+        .autoconnect()
+    
     var body: some View {
-        HStack {
-            Button {
-                isPlaying.toggle()
-                if isPlaying {
-                    playSoundURL(song.url)
-                } else {
-                    stopSound()
+//        VStack{
+            HStack {
+                Button {
+                    isPlaying.toggle()
+                    if isPlaying {
+//                        playSoundURL(song.url)
+                        AudioManager.instance.playSound(song.url, player: &player)
+                    } else {
+//                        stopSound()
+                        AudioManager.instance.stopSound(player: &player)
+                    }
+                } label: {
+                    Image(systemName: isPlaying ? "stop.circle.fill" : "play.circle.fill")
+                        .font(.system(size: 50, weight: .medium))
+                        .foregroundColor(.customGray)
+                        .padding(.bottom, 20)
                 }
-            } label: {
-                Image(systemName: isPlaying ? "stop.circle.fill" : "play.circle.fill")
-                    .font(.system(size: 50, weight: .medium))
-                    .foregroundColor(.customGray)
-                    .padding(.bottom, 20)
             }
-        }
-        .frame(maxWidth: .infinity)
-        .background(Color.HalmacSub)
-        .onDisappear(){
-            stopSound()
-        }
-        .onAppear{
-            playSoundURL(song.url)
-        }
+            .frame(maxWidth: .infinity)
+            .background(Color.HalmacSub)
+            .onDisappear(){
+//                stopSound()
+                AudioManager.instance.stopSound(player: &player)
+            }
+            .onAppear(){
+//                playSoundURL(song.url)
+                AudioManager.instance.playSound(song.url, player: &player)
+            }
+//        }.onReceive(timer) {
+//            value = player.currentTime
+//        }
     }
     
+/*
     func playSoundURL(_ urlString : String?) {
         
         guard let urlString = urlString else { fatalError("url을 받아올 수 없습니다.") }
         
         guard let url = URL(string: urlString) else { fatalError("url을 변환할 수 없습니다.") }
         let item = AVPlayerItem(url: url)
-        audioPlayer = AVPlayer(playerItem: item)
+        
+        player = AVPlayer(playerItem: item)
+        
         do {
             try AVAudioSession.sharedInstance().setCategory(.playback)
         } catch(let error) {
             print(error.localizedDescription)
         }
-        audioPlayer.play()
+        player.play()
     }
     
     func stopSound(){
-        guard let player = audioPlayer else {
-            return
+        player.pause()
+    }
+ */
+}
+
+final class AudioManager: ObservableObject {
+    static let instance = AudioManager()
+    var player: AVPlayer?
+    
+    @Published private(set) var isPlaying: Bool = false {
+        didSet{
+            print("isPlaying", isPlaying)
         }
+    }
+    
+    
+    func playSound(_ urlString : String?, player: inout AVPlayer) {
+//        guard let url = Bundle.main.url(forResource: "Tada", withExtension: ".mp3") else {return}
+//
+//        do {
+//        player = try AVAudioPlayer(contentsOf: url)
+//            player?.play()
+//        } catch let error {
+//            print("Error playing sound. \(error.localizedDescription)")
+//
+//        }
+        
+        guard let urlString = urlString else { fatalError("url을 받아올 수 없습니다.") }
+        guard let url = URL(string: urlString) else { fatalError("url을 변환할 수 없습니다.") }
+        let item = AVPlayerItem(url: url)
+        
+        player = AVPlayer(playerItem: item)
+        
+        do {
+            try AVAudioSession.sharedInstance().setCategory(.playback)
+        } catch(let error) {
+            print(error.localizedDescription)
+        }
+        player.play()
+    }
+    
+    func stopSound(player: inout AVPlayer) {
         player.pause()
     }
 }
