9b0f57ca6abb761c9b30dd2af0a1a744c6050569
jeonjimin <cjjunn0827@gmail.com>
2023-11-24 15:04:18 +0900

    [Feat] playlist move 수정

--- Halmap/View/Component/Progressbar.swift
+++ Halmap/View/Component/Progressbar.swift
@@ -1,18 +1,19 @@
 //
-//  Progressbar.swift
+//  Progress.swift
 //  Halmap
 //
-//  Created by JeonJimin on 2023/06/29.
+//  Created by JeonJimin on 11/24/23.
 //
-
 import SwiftUI
+import AVFoundation
 
-struct Progressbar: View {
-    @EnvironmentObject var audioManager: AudioManager
+struct ProgressBar: View {
+    let player: AVPlayer
     @Binding var team: String
     let isThumbActive: Bool
     
-    init(team: Binding<String>, isThumbActive: Bool) {
+    init(player: AVPlayer, team: Binding<String>, isThumbActive: Bool) {
+        self.player = player
         self.isThumbActive = isThumbActive
         self._team = team
         let thumbImage = makeThumbView(isThumbActive: isThumbActive)
@@ -21,32 +22,26 @@ struct Progressbar: View {
     }
     
     var body: some View {
-        VStack(spacing: 0) {
-            Slider(value: $audioManager.progressValue) { editing in
-                audioManager.didSliderChanged(editing)
-            }
-            .tint(Color("\(team)Point"))
-            .padding(.horizontal, isThumbActive ? 5 : 0)
-            .onChange(of: team) { _ in
-                let thumbImage = makeThumbView(isThumbActive: isThumbActive)
-                UISlider.appearance().setThumbImage(thumbImage, for: .normal)
-            }
-//            .disabled(!isThumbActive)
-            if isThumbActive {
-                HStack {
-                    Text(formatTime(audioManager.currentTime))
-                    Spacer()
-                    Text(formatTime(audioManager.duration))
-                }
-                .font(Font.Halmap.CustomCaptionMedium)
-                .foregroundColor(.customGray)
-            }
+        VStack {
+            AudioPlayerControlsView(player: player,
+                                    timeObserver: PlayerTimeObserver(player: player),
+                                    durationObserver: PlayerDurationObserver(player: player),
+                                    itemObserver: PlayerItemObserver(player: player),
+                                    isThumbActive: isThumbActive)
+            
         }
-        
-        
+        .tint(Color("\(team)Point"))
+        .padding(.horizontal, isThumbActive ? 5 : 0)
+        .onChange(of: team) { _ in
+            let thumbImage = makeThumbView(isThumbActive: isThumbActive)
+            UISlider.appearance().setThumbImage(thumbImage, for: .normal)
+        }
+        .onDisappear {
+            self.player.replaceCurrentItem(with: nil)
+        }
     }
     
-    func makeThumbView(isThumbActive: Bool) -> UIImage {
+    private func makeThumbView(isThumbActive: Bool) -> UIImage {
         lazy var thumbView: UIView = {
             let thumb = UIView()
             thumb.backgroundColor = UIColor(isThumbActive ? Color("\(team)Point") : Color.clear)
@@ -62,15 +57,139 @@ struct Progressbar: View {
         
         return image
     }
+}
+
+struct AudioPlayerControlsView: View {
+    private enum PlaybackState: Int {
+        case pause
+        case buffering
+        case playing
+    }
     
-    func formatTime(_ time: TimeInterval) -> String {
-        if time.isNaN {
-            return "00:00"
-        } else {
-            let minutes = Int(time) == 0 ? 0 : Int(time) / 60
-            let seconds = Int(time) == 0 ? 0 : Int(time) % 60
-            return String(format: "%02d:%02d", minutes, seconds)
+    let player: AVPlayer
+    let timeObserver: PlayerTimeObserver
+    let durationObserver: PlayerDurationObserver
+    let itemObserver: PlayerItemObserver
+    @State private var currentTime: TimeInterval = 0
+    @State private var currentDuration: TimeInterval = 0
+    @State private var state = PlaybackState.pause
+    
+    let isThumbActive: Bool
+    
+    var body: some View {
+        VStack {
+            Slider(value: $currentTime,
+                   in: 0...currentDuration,
+                   onEditingChanged: sliderEditingChanged)
+            .disabled(state != .playing)
+            .onReceive(timeObserver.publisher) { time in
+                self.currentTime = time
+                if time > 0 {
+                    self.state = .playing
+                }
+            }
+            .onReceive(durationObserver.publisher) { duration in
+                // Update the local var
+                self.currentDuration = duration
+            }
+            .onReceive(itemObserver.publisher) { hasItem in
+                self.state = hasItem ? .buffering : .pause
+                self.currentTime = 0
+                self.currentDuration = 0
+            }
+            
+            if isThumbActive {
+                HStack {
+                    Text(Utility.formatSecondsToHMS(currentTime))
+                    Spacer()
+                    Text(Utility.formatSecondsToHMS(currentDuration))
+                }
+                .font(Font.Halmap.CustomCaptionMedium)
+                .foregroundColor(.customGray)
+            }
         }
+    }
+    
+    // MARK: Private functions
+    private func sliderEditingChanged(editingStarted: Bool) {
+        if editingStarted {
+            timeObserver.pause(true)
+        }
+        else {
+            state = .buffering
+            let targetTime = CMTime(seconds: currentTime,
+                                    preferredTimescale: 600)
+            player.seek(to: targetTime) { _ in
+                self.timeObserver.pause(false)
+                self.state = .playing
+            }
+        }
+    }
+}
+
+import Combine
+class PlayerTimeObserver {
+    let publisher = PassthroughSubject<TimeInterval, Never>()
+    private weak var player: AVPlayer?
+    private var timeObservation: Any?
+    private var paused = false
+    
+    init(player: AVPlayer) {
+        self.player = player
         
+        timeObservation = player.addPeriodicTimeObserver(forInterval: CMTime(seconds: 0.5, preferredTimescale: 600), queue: nil) { [weak self] time in
+            guard let self = self else { return }
+            guard !self.paused else { return }
+            self.publisher.send(time.seconds)
+        }
+    }
+    
+    deinit {
+        if let player = player,
+            let observer = timeObservation {
+            player.removeTimeObserver(observer)
+        }
+    }
+    
+    func pause(_ pause: Bool) {
+        paused = pause
+    }
+}
+
+class PlayerItemObserver {
+    let publisher = PassthroughSubject<Bool, Never>()
+    private var itemObservation: NSKeyValueObservation?
+    
+    init(player: AVPlayer) {
+        // Observe the current item changing
+        itemObservation = player.observe(\.currentItem) { [weak self] player, change in
+            guard let self = self else { return }
+            // Publish whether the player has an item or not
+            self.publisher.send(player.currentItem != nil)
+        }
+    }
+    
+    deinit {
+        if let observer = itemObservation {
+            observer.invalidate()
+        }
+    }
+}
+
+class PlayerDurationObserver {
+    let publisher = PassthroughSubject<TimeInterval, Never>()
+    private var cancellable: AnyCancellable?
+    
+    init(player: AVPlayer) {
+        let durationKeyPath: KeyPath<AVPlayer, CMTime?> = \.currentItem?.duration
+        cancellable = player.publisher(for: durationKeyPath).sink { duration in
+            guard let duration = duration else { return }
+            guard duration.isNumeric else { return }
+            self.publisher.send(duration.seconds)
+        }
+    }
+    
+    deinit {
+        cancellable?.cancel()
     }
 }
---
+++ Halmap/View/Component/Utility.swift
@@ -0,0 +1,8 @@
+//
+//  Utility.swift
+//  Halmap
+//
+//  Created by JeonJimin on 11/24/23.
+//
+
+import Foundation
